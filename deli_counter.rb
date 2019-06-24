465f9c4e9169a0421bb425ad19b154e8782839f3
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-24 12:32:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def line (katz_deli)
+  
+  if katz_deli.length >= 1
+    new_array=[]
+    katz_deli.each do |name|
+      new_array.push("#{katz_deli.index(name) + 1}.")
+      new_array.push(name)
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+  else 
+    puts "The line is currently empty."
+  end
+
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  
+  puts "Welcome, #{name}. You are number #{position + 1} in line."
+  return name, position
+end
+
+
+
